5096074a941e656c42173dce96d82901d6b9138c
Lluís <lluis.revilla@gmail.com>
2019-11-16 12:53:41 +0100

    Rename and improve codes

---
+++ R/codes.R
@@ -0,0 +1,87 @@
+journal <- "BOE"
+
+# Heleper function
+add <- function(x) {
+    paste0("-", x)
+}
+
+#' Create the id for sumario
+#'
+#' @param date Date of the sumario
+#' @return A character vector
+#' @seealso [sumario_nbo]
+#' @examples
+#' sumario_xml(Sys.Date())
+#' sumario_xml(format(as.Date("2009/01/01", "%Y/%m/%d"), "%Y%m%d"))
+sumario_xml <- function(date) {
+    if (is(date, "Date")) {
+        date <- format(date, "%Y%m%d")
+    }
+    if (nchar(date) != 8  & !is_numeric(date)) {
+        stop("The date should be the in numeric YYYYMMDD format", call. = FALSE)
+    }
+    paste0(journal, add("S"), add(date))
+}
+
+#' Create the number of the sumario
+#'
+#' @param year Character or numeric value of the year of the summary in YYYY format.
+#' @param number Number of the summary in NNN format.
+#' @return A character vector
+#' @seealso [sumario_xml]
+#' @examples
+#' sumario_nbo(2019, 242)
+sumario_nbo <- function(year, number) {
+    if (nchar(year) != 4  & !is_numeric(year)) {
+        stop("The year should be the in numeric YYYY format", call. = FALSE)
+    }
+    if (nchar(number) != 4  & !is_numeric(number)) {
+        stop("The number should be the in numeric NNN format", call. = FALSE)
+    }
+    paste0(journal, add("S"), add(year), add(number))
+}
+
+#' @name element
+#' @title Elements: Disposición and anuncio
+#'
+#' @param year Character or numeric value of the year of the element in YYYY format.
+#' @param number Character or numeric value of the element.
+#' @return A character vector
+NULL
+
+elemento <- function(item = c("B", "A"), year, number) {
+    item <- match.arg(item, c("B", "A"))
+    if (nchar(year) != 4  & !is_numeric(year)) {
+        stop("The year should be in numeric YYYY format", call. = FALSE)
+    }
+    if (nchar(number) > 6  & !is_numeric(number)) {
+        stop("The year should be in numeric XXXXXX format", call. = FALSE)
+    }
+    paste0(journal, add(item), add(year), add(number))
+}
+
+#' @describeIn element Create the number of the diposicion
+#' @examples
+#' disposicion(2019, 242)
+disposicion <- function(year, number) {
+    elemento(item = "B", year = year, number = number)
+}
+
+#' @describeIn element Create the number of the anuncio
+#' @examples
+#' anuncio(2019, 242)
+anuncio <- function(year, number) {
+    elemento(item = "A", year = year, number = number)
+}
+
+is_numeric <- function(x){
+    tryCatch(as.numeric(x), warning = function(w){FALSE}, finally = TRUE)
+}
+
+strs <- function(code) {
+    strsplit(code, "-", fixed = TRUE)
+}
+
+len <- function(splits) {
+    lapply(splits, nchar)
+}

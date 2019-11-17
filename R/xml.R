b28bf1b9a109a860b75b3fe9ac1828f251aa84a7
Lluís <lluis.revilla@gmail.com>
2019-11-17 16:41:49 +0100

    Remove this file as it is no longer needed

--- R/xml.R
+++
@@ -1,7 +0,0 @@
-#' @importFrom xml2 read_xml
-NULL
-
-
-# xm <- read_xml("https://boe.es/diario_boe/xml.php?id=BOE-S-20141006")
-
-# h <- httr::modify_url("https://boe.es/diario_boe/xml.php", query = c("id=BOE-S-20141006"))

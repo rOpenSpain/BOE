b74aa4c22490224e635eb47ca535769b9ee4ec3d
Lluís <lluis.revilla@gmail.com>
2019-12-02 23:29:42 +0100

    Test the retrieve code

---
+++ tests/testthat/test-retrieve.R
@@ -0,0 +1,15 @@
+test_that("retreive_sumario works", {
+    skip_if_offline()
+    sumario <- retrieve_sumario(as.Date("2019-12-02", "%Y-%m-%d"))
+    expect_s3_class(sumario, "data.frame")
+
+    expect_error(retrieve_sumario(as.Date("2019-12-01", "%Y-%m-%d")), "The date")
+
+})
+
+test_that("url_publications works", {
+    skip_if_offline()
+    sumario <- retrieve_sumario(as.Date("2019-12-02", "%Y-%m-%d"))
+    urls <- url_publications(sumario)
+    expect_length(urls, nrow(sumario))
+})

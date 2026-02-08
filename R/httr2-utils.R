80297195ddfc7aacc48f9c33ff9aeaa05a87937d
Lluís <lluis.revilla@gmail.com>
2026-02-08 17:37:47 +0100

    parametrize the function differently

--- R/httr2-utils.R
+++ R/httr2-utils.R
@@ -4,11 +4,11 @@
 #' @importFrom httr2 req_url_query
 #' @importFrom httr2 req_throttle
 #' @importFrom httr2 req_retry
-req_api <- function(params) {
-  request("https://boe.es/datosabiertos/api/") |> 
+req_api <- function(...) {
+  request("https://boe.es/datosabiertos/api/") |>
     req_headers("Accept" = "application/xml") |>
-    req_user_agent("BOE (https://ropenspain.github.io/BOE)") |> 
-    req_url_query(!!!params) |>
-    req_throttle(capacity = 30) |> 
+    req_user_agent("BOE (https://ropenspain.github.io/BOE)") |>
+    req_url_query(...) |>
+    req_throttle(capacity = 30) |>
     req_retry(max_tries = 5, backoff = function(x) { x })
 }

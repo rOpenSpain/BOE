3ed607aa0388cf8f9cf82035f4ac4fbc159438a5
Lluís <lluis.revilla@gmail.com>
2026-02-08 17:37:04 +0100

    Add some more checks about the new parameters

--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,30 @@ format_dates <- function(x) {
     }
     x
 }
+
+format_date <- function(date){
+    if (is(date, "Date")) {
+        date <- format(date, "%Y%m%d")
+    } else if (is.numeric(date)) {
+        date <- as.character(date)
+    }
+    date
+}
+
+check_limit <- function(limit) {
+    if (!is.numeric(limit)) {
+        stop("Limit must be a numeric number from -1 to higher numbers", call. = FALSE)
+    }
+    if (limit < -1L) {
+        stop("Limit can only be between [-1, Inf]", call. = FALSE)
+    }
+}
+
+check_offset <- function(offset) {
+    if (!is.numeric(offset)) {
+        stop("Offset must be a numeric number higher than 1.", call. = FALSE)
+    }
+    if (offset <= 0L) {
+        stop("Offset must be a postivie number", call. = FALSE)
+    }
+}
